a171fd432add26d6f47b4314947716eba634e054
Michael Ozoudeh <jesmikky@gmail.com>
2025-10-23 01:36:42 +0100

    order fixed

--- app/Mail/DispatchMail.php
+++ app/Mail/DispatchMail.php
@@ -39,7 +39,7 @@ class DispatchMail extends Mailable
     public function content(): Content
     {
           return new Content(
-            view: 'mails.DispatchMail',
+            view: 'mails.dispatchMail',
             with: [
                 'data' => $this->data
             ]
